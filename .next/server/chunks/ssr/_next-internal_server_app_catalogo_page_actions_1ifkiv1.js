e12489779cc4981c69fbff9366ed58d48948dad5
M4RQUIT0S <marcosobrido@gmail.com>
2026-08-26 18:27:13 -0300

    feat: add TypeScript definitions for Next.js routes and parameters
    - Create root-params.d.ts for root parameter types.
    - Generate routes.d.ts to define application routes and their parameters.
    - Implement validator.ts to validate page and layout exports.
    - Add .vercel configuration files for project settings and environment variables.
    - Include next-env.d.ts for Next.js type references.

---
+++ .next/server/chunks/ssr/_next-internal_server_app_catalogo_page_actions_1ifkiv1.js
@@ -0,0 +1,3 @@
+module.exports=[40188,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_catalogo_page_actions_1ifkiv1.js.map
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MQ5uR84d16xVKKRvya3hY+AnK/QvoyD4+mFCGAswpJE=\"\n}"
